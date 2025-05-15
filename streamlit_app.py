cf3711b6d20cc728c882b3f42ce4c1181e705d6c
Alopez881 <155206985+Alopez881@users.noreply.github.com>
2025-05-14 21:22:25 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ st.info('This is app buildis a machine learning model!')
 
 with st.expander('Data'):
   st.write('**Raw data**')
-  df = pd.read.csv('https://raw.githubusercontent.com/Nissi-d/sentiment-analysis/refs/heads/main/data/raw/reviews.csv')
+  df = pd.read.csv('https://raw.githubusercontent.com/Alopez881/ECS171-MachineLearning/refs/heads/master/reviews.csv')
   df

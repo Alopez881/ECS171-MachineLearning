ab2debdf8edb9119d88888aa674494421ae1fd19
Alopez881 <155206985+Alopez881@users.noreply.github.com>
2025-05-14 21:30:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,9 @@ with st.expander('Data'):
   df
 
   st.write('**X**')
-  X = df.drop('species', axis=1)
+  X = df.drop('score', axis=1)
   X
 
   st.write('**y**')
-  y = df.species
+  y = df.score
   y

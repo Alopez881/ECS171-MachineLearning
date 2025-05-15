ab05941d4fcca45fec1635e22b1c054f90512b05
Alopez881 <155206985+Alopez881@users.noreply.github.com>
2025-05-14 21:26:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,11 @@ with st.expander('Data'):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/Alopez881/ECS171-MachineLearning/refs/heads/master/reviews.csv')
   df
+
+  st.write('**X**')
+  X = df.drop('species', axis=1)
+  X
+
+  st.write('**y**')
+  y = df.species
+  y
